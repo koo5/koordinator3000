10125dabb1acd5073d5bcbd08f0fc50f158205ed
koo5 <kolman.jindrich@gmail.com>
2020-12-31 05:28:06 +0100

    ..

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import compression from "compression";
 import express, { Express } from "express";
 import sirv from "sirv";
 import { createApolloServer } from "./graphql";
+import { auth } from 'express-openid-connect'
 
 const PORT = process.env.PORT; // eslint-disable-line prefer-destructuring
 const mode = process.env.NODE_ENV;
@@ -14,7 +15,6 @@ const createSapperAndApolloServer = async (graphqlPath = "/graphql"): Promise<Ex
 	const app = express();
 
 	const apolloServer = await createApolloServer();
-
 	apolloServer.applyMiddleware({ app, path: graphqlPath });
 
 	if (main) {
@@ -23,6 +23,24 @@ const createSapperAndApolloServer = async (graphqlPath = "/graphql"): Promise<Ex
 
 	app.use(
 		compression({ threshold: 0 }),
+		auth({
+		  required: false,
+		  auth0Logout: true,
+		  baseURL: 'http://localhost:3000',
+		  issuerBaseURL: 'https://your-hosted-url.auth0.com',
+		  clientID: 'your-client-id',
+		  appSession: {secret: 'your-session-secret'}
+		}),
+		(req, res, next) => {
+		  return sapper.middleware({
+			session: () => {
+			  return {
+				isAuthenticated: req.isAuthenticated(),
+				  bla:"bla",
+				user: req.openid.user
+			  }
+			}
+		  })(req, res, next)},
 		sapper.middleware(),
 	);
 
