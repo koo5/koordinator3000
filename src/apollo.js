9c7809b0f82537b0cceba0aaeb8b198c78774677
koo5 <kolman.jindrich@gmail.com>
2020-12-31 03:15:51 +0100

    ..

--- src/apollo.js
+++ src/apollo.js
@@ -1,9 +1,11 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { WebSocketLink } from "apollo-link-ws";
 import { split } from "apollo-link";
 import { HttpLink } from "apollo-link-http";
 import { getMainDefinition } from "apollo-utilities";
+import fetch from 'node-fetch';
+
 
 const headers = {'content-type': 'application/json'};
 
@@ -13,7 +15,7 @@ const getHeaders = () => {
 
 const cache = new InMemoryCache();
 
-const wsLink = new WebSocketLink({
+const wsLink = process.browser ? new WebSocketLink({
   uri: "ws://koordinator2.hasura.app/v1/graphql",
   options: {
     reconnect: true,
@@ -22,21 +24,23 @@ const wsLink = new WebSocketLink({
       return { headers: getHeaders() };
     },
   },
-});
+}) : null;
 
 const httpLink = new HttpLink({
   uri: "https://koordinator2.hasura.app/v1/graphql",
-  headers: getHeaders()
+  headers: getHeaders(),
+  fetch:fetch,
+
 });
 
-const link = split(
+const link = process.browser ? split(
   ({ query }) => {
     const { kind, operation } = getMainDefinition(query);
-    return kind === "OperationDefinition" && operation === "subscription";
+    return kind === "OperationDefinition" && (operation === "subscription" || operation === "mutation");
   },
   wsLink,
   httpLink
-);
+) : httpLink;
 
 export const client = new ApolloClient({
   ssrMode: true,
